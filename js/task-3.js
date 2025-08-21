a6ece36210ab3328caea2f3545299389a082d773
Sergiy4ik <sergiy063l@gmail.com>
2025-08-21 13:03:24 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,15 @@
-function getElementWidth(content, padding, border) {
-    const contentValue = parseFloat(content);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-    return contentValue + paddingValue * 2 + borderValue * 2;
+function filterArray(numbers, value) {
+    let = trueNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+trueNumbers.push(numbers[i])
+        }
+    }
+    return trueNumbers
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
